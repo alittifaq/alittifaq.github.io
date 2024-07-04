58ed364e678a319d05ff08020bb5656c01ef6ede
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-07-04 21:10:08 +0700

    coba tambahan kuesioner

--- js/gallery.js
+++ js/gallery.js
@@ -1,23 +1,34 @@
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import { setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 
+// Mendefinisikan URL untuk data galeri
 const urlgallery = "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery";
+// Memanggil fungsi getJSON untuk mengambil data dari urlgallery
+// "login" dan "" adalah parameter tambahan untuk getJSON (biasanya untuk autentikasi)
 getJSON(urlgallery, "login", "", getGalleryFunction);
 
+// Fungsi callback yang akan dijalankan setelah data berhasil diambil
 function getGalleryFunction(result) {
+   // Menyimpan data galeri dari hasil yang diterima
   let gallery = result.data;
+  // Menyimpan HTML galeri yang akan dibuat
   let htmlgallery = "";
+  // Iterasi melalui setiap item dalam data galeri
   gallery.forEach(function (gallery) {
+    // Mengganti placeholder dalam template figuregallery dengan data aktual
     let tem = figuregallery
       .replace("##foto##", gallery.foto)
       .replace("##judul_kegiatan##", gallery.judul_kegiatan)
       .replace("##tahun##", gallery.tahun);
+      // Menambahkan setiap item galeri yang dihasilkan ke dalam variabel htmlgallery.
     htmlgallery += tem;
+    // Menampilkan data galeri di konsol untuk debugging
     console.log(gallery);
   });
+  // Mengatur konten HTML elemen dengan ID 'gallery' dengan htmlgallery yang dihasilkan
   setInner("gallery", htmlgallery);
 }
-
+// Template HTML untuk setiap item galeri
 let figuregallery = `
 <figure class="gallery-item">
             <img id="foto" src="##foto##" alt="Kegiatan 1" />
--- js/main.js
+++ js/main.js
@@ -1,107 +1,124 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    // Menjalankan fungsi setelah delay 1 milidetik
+    setTimeout(function () {
+      // Mengecek apakah elemen dengan id 'spinner' ada di halaman
+      if ($("#spinner").length > 0) {
+        // Menghapus kelas 'show' dari elemen dengan id 'spinner'
+        $("#spinner").removeClass("show");
+      }
+    }, 1); // Delay 1 milidetik
+  };
 
+  // Memanggil fungsi spinner
+  spinner();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-150px');
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  // Initiate the wowjs
+  // Memulai wow.js untuk animasi saat elemen masuk ke viewport
+  new WOW().init();
 
-    // Gallery Hidden
-    document.getElementById('loadMoreBtn').addEventListener('click', function() {
-        var hiddenItems = document.querySelectorAll('.gallery-item.hidden');
-        hiddenItems.forEach(function(item) {
-          item.classList.remove('hidden');
-        });
-        this.style.display = 'none';
-    });
+  // Sticky Navbar
+  // Menambahkan efek sticky pada navbar
+  $(window).scroll(function () {
+    // Jika posisi scroll lebih dari 300px
+    if ($(this).scrollTop() > 300) {
+      // Menambahkan kelas 'shadow-sm' dan mengatur posisi top menjadi 0px
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      // Menghapus kelas 'shadow-sm' dan mengatur posisi top menjadi -150px
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-150px");
+    }
+  });
+
+  // Back to top button
+  // Menampilkan tombol "back to top" saat posisi scroll lebih dari 300px
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      // Menampilkan tombol dengan efek fade in
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      // Menyembunyikan tombol dengan efek fade out
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
 
 
-    // Modal Video
-    var $videoSrc;
-    $('.btn-play').click(function () {
-        $videoSrc = $(this).data("src");
-    });
-    console.log($videoSrc);
-    $('#videoModal').on('shown.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
-    $('#videoModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src', $videoSrc);
-    })
+  // Menambahkan fungsi klik pada tombol "back to top"
+  $(".back-to-top").click(function () {
+    // Menggulir halaman kembali ke atas dengan animasi
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false; // Mencegah aksi default dari link
+  });
 
-
-    // Product carousel
-    $(".product-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        loop: true,
-        center: true,
-        dots: false,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-			0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
-    // Testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        loop: true,
-        dots: true,
-        nav: false,
-    });
-    
+  // Gallery Hidden
+ // Menambahkan event listener pada tombol "Load More"
+ document.getElementById("loadMoreBtn").addEventListener("click", function () {
+  // Mendapatkan semua elemen galeri yang tersembunyi
+  var hiddenItems = document.querySelectorAll(".gallery-item.hidden");
+  // Menampilkan setiap item galeri yang tersembunyi
+  hiddenItems.forEach(function (item) {
+    item.classList.remove("hidden");
+  });
+  // Menyembunyikan tombol "Load More" setelah diklik
+  this.style.display = "none";
+});
 })(jQuery);
 
+  // Modal Video
+  var $videoSrc;
+  $(".btn-play").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  $("#videoModal").on("shown.bs.modal", function (e) {
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+  $("#videoModal").on("hide.bs.modal", function (e) {
+    $("#video").attr("src", $videoSrc);
+  });
+
+  // Product carousel
+  $(".product-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    loop: true,
+    center: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  // Testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    loop: true,
+    dots: true,
+    nav: false,
+  });
+})(jQuery);
